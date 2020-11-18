70d4783d51d205fcb859609ccbeafc554da456f5
Akshu Aggarwal <akshuaggarwal25@gmail.com>
2020-11-18 16:32:39 +0530

    mutilple operand

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ var ar = [];
 document.addEventListener('keyup', function (event) {
     var kc = event.keyCode;
     var vali = /^[0-9/%*+.-]$/;
+   // var oper =/^[/*+%-.]$/;
+    var lastvalue=ar[ar.length -1];
     
     if(ar.length==0){
         var opera =/^[/*+]$/;
@@ -18,7 +20,7 @@ document.addEventListener('keyup', function (event) {
                 document.getElementById('res').innerHTML = eval(ar.join(""));
             }
             
-            else if (kc == 8) {
+            else if (kc == 8) {//for backspace 
                 
                 const arrString = document.getElementById("calu").innerText.split("");
                 alert(arrString);
@@ -32,7 +34,7 @@ document.addEventListener('keyup', function (event) {
                     document.getElementById('res').innerText = ar.join("");
                 }
             }
-            else if(kc==13){
+            else if(kc==13){ //for enter
                 let res1 = document.getElementById('res').innerHTML;
                 let calu1 = document.getElementById('calu').innerHTML=" ";
                 let forcss = document.getElementById('calu').style;
@@ -40,10 +42,15 @@ document.addEventListener('keyup', function (event) {
                
                 document.getElementById('res').innerHTML = calu1;
                 document.getElementById('calu').innerHTML = res1;
-            
-            }      
-            
-            
+               
+            }
+            else if(lastvalue=='+'){
+                newval='';
+                repalval=ar.fill(newval,-1);
+             //   document.getElementById('calu').innerHTML=repalval;
+                document.getElementById('calu').innerText = ar.join("");
+
+            }
         }
     }else{
         if (vali.test(event.key)) {
@@ -68,7 +75,7 @@ document.addEventListener('keyup', function (event) {
                 document.getElementById('res').innerText = ar.join("");
             }
         }
-        else if( kc==13){//for enter key
+        else if(kc==13){//for enter key
             let res1 = document.getElementById('res').innerHTML;
             let calu1 = document.getElementById('calu').innerHTML=" ";
             let forcss = document.getElementById('calu').style;
@@ -76,10 +83,18 @@ document.addEventListener('keyup', function (event) {
            
           document.getElementById('res').innerHTML = calu1;
             document.getElementById('calu').innerHTML = res1;
-        
-        }   
+           
+        }  
+        else if(lastvalue=='+'){
+            newval='';
+            repalval=ar.fill(newval,-1);
+            //document.getElementById('calu').innerHTML=repalval;
+            document.getElementById('calu').innerText = ar.join("");
+
+        }
     }
 });
+var res2 = "";
 function container(val) {
     if (val.value == "=") {
         
@@ -91,22 +106,35 @@ function container(val) {
         document.getElementById('res').innerHTML = calu2;
         document.getElementById('calu').innerHTML = res2;
     } else {
-
-        se = val.value;
-        let newval = document.getElementById('calu').innerHTML;
-        res2 = document.getElementById('calu').innerHTML = newval + se;
-        // document.getElementById('res').innerHTML = eval(res2);
         
-        //var opera1 =/^[0-9]$/;
-        var opera1 =/^[/*+]$/;
-       console.log("abc -==========> ",opera1.test(res2.slice(-1)));
-        // if(opera1.test(res2.slice(-1))){
-        //     alert('Numbers');
-        // }
-        // else{
-        //     alert('operend');
-        // }
-
+		//console.log("==============>"+eval(res2));
+        //document.getElementById('res').innerHTML = eval(res2);
+		
+        var opera1 =/^[/*+%-.]$/;
+        if(opera1.test(res2.slice(-1))){
+            if(!(opera1.test(val.value))){
+				console.log("operand")
+                se = val.value;
+                let newval = document.getElementById('calu').innerHTML;
+                res2 = document.getElementById('calu').innerHTML = newval + se;
+			    console.log("==============>"+eval(res2));
+			    document.getElementById('res').innerHTML = eval(res2);
+			}else{
+				  //console.log(res2[res2.length-1])
+				  res2 = res2.replace(res2[res2.length-1], val.value);
+				  document.getElementById('calu').innerHTML = res2;
+				  //console.log("Last operator is======>>>"+res2[res2.length-1]+"index=>"+res2.length+"  should be replace with ======>"+val.value);
+				  //res2[res2.length] = val.value;
+			}
+         } else{
+			 console.log("operand")
+             se = val.value;
+             let newval = document.getElementById('calu').innerHTML;
+             res2 = document.getElementById('calu').innerHTML = newval + se;
+			 console.log("==============>"+eval(res2));
+			 document.getElementById('res').innerHTML = eval(res2);
+         }
+        
     }
 }
 function allclear() {
@@ -124,3 +152,16 @@ function clear1() {
             document.getElementById('res').innerText = ar.join("");
         }
 }
+
+//console.log('operator');
+            //alert(typeof(res2));
+            //var new1 = res2.fill(replacevalue, -1);
+            //document.getElementById('res').innerHTML =new1;
+		/*	 else{
+			 console.log("operand")
+             se = val.value;
+             let newval = document.getElementById('calu').innerHTML;
+             res2 = document.getElementById('calu').innerHTML = newval + se;
+			 console.log("==============>"+eval(res2));
+			 document.getElementById('res').innerHTML = eval(res2);
+         }//ek min */
